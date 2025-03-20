644e50345a2491658890bd4af96ce2815830dd02
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-03-20 04:21:17 +0000

    work in progress

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -53,7 +53,9 @@ class CommentsController < ApplicationController
     @comment.destroy
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
-      format.json { head :no_content }
+      format.js do
+        render template: "comments/destroy"
+      end
     end
   end
 
